e56e4977c124cb953d81d2b74030aa5b0f15a6b7
vlad2876 <vlad28761@gmail.com>
2020-09-05 19:21:59 +0300

    .

--- src/by/belhard/j24/homeworks/MyPerson.java
+++ src/by/belhard/j24/homeworks/MyPerson.java
@@ -4,9 +4,10 @@ public class MyPerson {
     public static void main(String[] args) {
 
         Person p1 = new Person("Stepan", 23, Sex.MALE, new Country("Russia", 238357));
-        System.out.println(p1.country.title);
-
-        Person p2 = new Person("Vasily", p1.age, Sex.MALE, new Country("UK", 488323));
-        System.out.println(p2.country.title);
+        Person p2 = new Person("Vasily", 36, Sex.MALE, new Country("UK", 488323));
+        p1.eat("Meat");
+        p2.growOld();
+        p2.walk();
+        System.out.println("Vasily is " + p2.age + " years old");
     }
 }
